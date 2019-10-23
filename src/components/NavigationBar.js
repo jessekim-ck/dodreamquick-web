0fa7cf4040dcfcf3e7335ad45b29f3dd9f197879
jessekim <jessekim.ck.94@gmail.com>
2019-10-23 20:12:59 +0900

    Show dropdown items when mouse hover

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -11,53 +11,80 @@ import {connect} from 'react-redux'
 import {log_out} from "../redux/actions/user_actions";
 
 
-const NavigationBar = props => {
+class NavigationBar extends React.Component {
 
-    const do_log_out = async () => {
-        await localStorage.removeItem('token')
-        await props.dispatch(log_out())
+    state = {
+        show_service_intro: false,
+        show_post: false,
     }
 
-    return (
-        <Navbar expand="lg" fixed="top" className={styles.navBarContainer}>
-            <Navbar.Brand href="/">
-                <img
-                    src={Logo}
-                    height="35"
-                    alt="두드림퀵"/>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/order">배송 신청</Nav.Link>
-                    <NavDropdown title="서비스 안내">
-                        <NavDropdown.Item href="/search_cost">요금 안내</NavDropdown.Item>
-                        <NavDropdown.Item href="/no_page_yet">이용 방법</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title="게시판">
+    on_mouse_hover = key => {
+        this.setState({
+            [`show_${key}`]: true
+        })
+    }
+
+    on_mouse_leave = key => {
+        this.setState({
+            [`show_${key}`]: false
+        })
+    }
+
+    do_log_out = async () => {
+        await localStorage.removeItem('token')
+        await this.props.dispatch(log_out())
+    }
+
+    render() {
+        return (
+            <Navbar expand="lg" fixed="top" className={styles.navBarContainer}>
+                <Navbar.Brand href="/">
+                    <img
+                        src={Logo}
+                        height="35"
+                        alt="두드림퀵"/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/order">배송 신청</Nav.Link>
+                        <NavDropdown
+                            title="서비스 안내"
+                            show={this.state.show_service_intro}
+                            onMouseEnter={() => this.on_mouse_hover('service_intro')}
+                            onMouseLeave={() => this.on_mouse_leave('service_intro')}>
+                            <NavDropdown.Item href="/search_cost">요금 안내</NavDropdown.Item>
+                            <NavDropdown.Item href="/no_page_yet">이용 방법</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown
+                            title="게시판"
+                            show={this.state.show_post}
+                            onMouseEnter={() => this.on_mouse_hover('post')}
+                            onMouseLeave={() => this.on_mouse_leave('post')}>
                         <NavDropdown.Item href="/post/notice">공지사항</NavDropdown.Item>
-                        <NavDropdown.Item href="/post/faq">FAQ</NavDropdown.Item>
-                        <NavDropdown.Item href="/post/news">언론 보도</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-            </Navbar.Collapse>
-            <Navbar.Collapse className="justify-content-end">
-                <div>
-                    {
-                        props.username ?
-                            <Nav className={"mr-auto"}>
-                                <Nav.Link onClick={do_log_out}>로그아웃</Nav.Link>
-                                <Nav.Link href="/my_page">내 정보</Nav.Link>
-                            </Nav> :
-                            <Nav className={"mr-auto"}>
-                                <Nav.Link onClick={() => props.open_modal('login')}>로그인</Nav.Link>
-                                <Nav.Link onClick={() => props.open_modal('signup')}>회원가입</Nav.Link>
-                            </Nav>
-                    }
-                </div>
-            </Navbar.Collapse>
-        </Navbar>
-    )
+                            <NavDropdown.Item href="/post/faq">FAQ</NavDropdown.Item>
+                            <NavDropdown.Item href="/post/news">언론 보도</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+                <Navbar.Collapse className="justify-content-end">
+                    <div>
+                        {
+                            this.props.username ?
+                                <Nav className={"mr-auto"}>
+                                    <Nav.Link onClick={this.do_log_out}>로그아웃</Nav.Link>
+                                    <Nav.Link href="/my_page">내 정보</Nav.Link>
+                                </Nav> :
+                                <Nav className={"mr-auto"}>
+                                    <Nav.Link onClick={() => this.props.open_modal('login')}>로그인</Nav.Link>
+                                    <Nav.Link onClick={() => this.props.open_modal('signup')}>회원가입</Nav.Link>
+                                </Nav>
+                        }
+                    </div>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
 }
 
 const mapStateToProps = state => state.user
