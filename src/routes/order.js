ae2bdd6826ef2aeb5694a56475cfecf29dde00fe
ikjune07 <ikjune.sur07@gmail.com>
2020-08-27 23:52:00 +0900

    changed the notice and re-opened thursday's orders

--- src/routes/order.js
+++ src/routes/order.js
@@ -25,8 +25,8 @@ class Order extends React.Component {
         // alert("배송 가능한 택배원이 많지 않아 부득이하게 주문을 닫습니다. 정말 죄송합니다.");
         // this.props.history.push("/");
         //alert("죄송합니다. 시스템 오류로 인해 잠시 주문접수를 중단합니다.")
-        if (current_day === 6 || current_day === 0 || current_day === 4 ) {
-            alert("태풍으로 인한 택배원 님들의 안전 문제로 배송이 불가능합니다 :) 내일 찾아주세요!");
+        if (current_day === 6 || current_day === 0) {
+            alert("공휴일 및 주말에는 주문이 불가능합니다 :) 내일 찾아주세요!");
             this.props.history.push("/");
         }  else if (current_hour < 9 || current_hour > 17) {
             alert("주문 불가능한 시간입니다! 두드림퀵 배송 신청은 오전 9시부터 오후 5시까지입니다.");
