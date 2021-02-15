81f83fc69d70e923e954f1a9f59cd3bef4c03b73
JerryPark <eaeao@naver.com>
2021-02-15 22:13:41 +0900

    applid qa requests

--- src/routes/order.js
+++ src/routes/order.js
@@ -7,7 +7,6 @@ import {connect} from "react-redux";
 import {get_setting} from "../redux/actions/common_actions";
 
 
-
 const ReactGA = require('react-ga');
 const caution_calendar = require("../assets/caution_calendar.png");
 const caution_clock = require("../assets/caution_clock.png");
@@ -42,24 +41,24 @@ class Order extends React.Component {
         open.setHours(Number(setting.order_open_time.slice(0, 2)), Number(setting.order_open_time.slice(3, 5)), 0)
         close.setHours(Number(setting.order_close_time.slice(0, 2)), Number(setting.order_close_time.slice(3, 5)), 0)
 
-        if (setting.order_never_open) {
-            this.setState({ is_opened: false });
+        if (setting.order_open_company_user && this.props.role === 'CO') {
+        } else if (setting.order_never_open) {
+            this.setState({is_opened: false});
             alert(close_message || "죄송합니다. 내부사정으로 인해 잠시 주문접수를 중단합니다.");
             this.props.history.goBack();
             return;
-        } else if (setting.order_open_company_user && this.props.role === 'CO') {
         } else if (current_day === 6 || current_day === 0) {
-            this.setState({ is_opened: false });
+            this.setState({is_opened: false});
             alert(close_message || "공휴일 및 주말에는 주문이 불가능합니다 :) 평일에 찾아주세요!");
             this.props.history.goBack();
             return;
         } else if (now < open || now > close) {
-            this.setState({ is_opened: false });
+            this.setState({is_opened: false});
             alert(close_message || "주문이 불가능한 시간입니다!두드림퀵 배송 신청은 오전 9시부터 오후 5시까지입니다.");
             this.props.history.goBack();
             return;
         }
-        this.setState({ is_opened: true });
+        this.setState({is_opened: true});
     }
 
     make_order = async order_data => {
@@ -87,13 +86,13 @@ class Order extends React.Component {
         const dueDateMonth = (new Date().getMonth() + 1) < 10 ? `0${new Date().getMonth() + 1}` : new Date().getMonth() + 1
         const dueDateDate = new Date().getDate() < 10 ? `0${new Date().getDate()}` : new Date().getDate()
         await IMP.request_pay({
-            pg : 'html5_inicis',
-            pay_method : order_data.pay_method,
-            merchant_uid : 'merchant_' + new Date().getTime(),
-            name : '두드림퀵 배송 주문 (' + order.id + ')',
-            amount : order_data.price,
-            buyer_name : order_data.sender_name,
-            buyer_tel : order_data.sender_phone,
+            pg: 'html5_inicis',
+            pay_method: order_data.pay_method,
+            merchant_uid: 'merchant_' + new Date().getTime(),
+            name: '두드림퀵 배송 주문 (' + order.id + ')',
+            amount: order_data.price,
+            buyer_name: order_data.sender_name,
+            buyer_tel: order_data.sender_phone,
             buyer_email: this.props.username,
             m_redirect_url: `https://dodreamquick.com/order/complete/${order.id}/${order_data.price}`,
             vbank_due: `${new Date().getFullYear()}${dueDateMonth}${dueDateDate}1730`
@@ -106,7 +105,7 @@ class Order extends React.Component {
                         // await completeOrderPayment(order_id, order_price)
                         this.props.history.push({
                             pathname: '/order/complete',
-                            state: { detail: validated }
+                            state: {detail: validated}
                         })
                     } else if (validated.status !== 'paid') {
                         alert("결제 승인이 완료되지 않았습니다. 두드림퀵 팀에 문의해주세요.")
@@ -132,15 +131,15 @@ class Order extends React.Component {
     }
 
     render() {
-        const { setting } = this.props;
+        const {setting} = this.props;
         return this.state.is_opened && setting ? (
             <div className={styles.contentContainer}>
                 <Helmet>
                     <title>두드림퀵: 배송 신청</title>
                     <meta
                         name="description"
-                        content="두드림퀵 지하철 퀵서비스 배송 신청" />
-                    <link rel="canonical" href="https://dodreamquick.com/order" />
+                        content="두드림퀵 지하철 퀵서비스 배송 신청"/>
+                    <link rel="canonical" href="https://dodreamquick.com/order"/>
                 </Helmet>
                 <div className={styles.orderCautionContainer}>
                     <div className={styles.orderCautionTitle}>
@@ -154,7 +153,8 @@ class Order extends React.Component {
                                 예약 주문 관련 안내
                             </div>
                             <div className={styles.orderCautionItemText}>
-                                즉시 배송이 아니라 픽업 시간을 지정하시고 싶으신 경우 반드시 <span>카카오톡 채널을 통해 사전 문의 바랍니다.</span> 문의되지 않은 예약 주문은 정상 진행되지 않을 수 있으며, 사전 고지 없이 환불될 수 있습니다.
+                                즉시 배송이 아니라 픽업 시간을 지정하시고 싶으신 경우 반드시 <span>카카오톡 채널을 통해 사전 문의 바랍니다.</span> 문의되지 않은 예약 주문은
+                                정상 진행되지 않을 수 있으며, 사전 고지 없이 환불될 수 있습니다.
                             </div>
                         </div>
 
